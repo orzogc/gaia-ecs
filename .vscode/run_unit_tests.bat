6709247d997223b3b78a0771f0cea162589a8fad
Richard Biely <richardbiely@gmail.com>
2026-04-17 20:51:06 +0200

    Fixed: run_unit_test.bat would not use the right path to binary

--- .vscode/run_unit_tests.bat
+++ .vscode/run_unit_tests.bat
@@ -5,7 +5,7 @@ set "BUILD_TYPE=%~1"
 if "%BUILD_TYPE%"=="" set "BUILD_TYPE=Debug"
 
 set "REPO_ROOT=%~dp0.."
-set "TEST_DIR=%REPO_ROOT%\build\%BUILD_TYPE%\src\test"
+set "TEST_DIR=%REPO_ROOT%\build\%BUILD_TYPE%\src\test\%BUILD_TYPE%\"
 
 call :run_test gaia_test.exe || exit /b 1
 call :run_test gaia_test_no_autoreg.exe --test-case=*component registration* || exit /b 1
