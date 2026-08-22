2843d22a930f3c1efc6b54c995065a1dacd7161a
Richard Biely <richardbiely@gmail.com>
2026-08-22 13:08:11 +0400

    chore(version): bump to 1.0.0

--- include/gaia/config/version.h
+++ include/gaia/config/version.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // Breaking changes and big features
-#define GAIA_VERSION_MAJOR 0
+#define GAIA_VERSION_MAJOR 1
 // Smaller changes and features
-#define GAIA_VERSION_MINOR 9
+#define GAIA_VERSION_MINOR 0
 // Fixes and tweaks
-#define GAIA_VERSION_PATCH 3
+#define GAIA_VERSION_PATCH 0
